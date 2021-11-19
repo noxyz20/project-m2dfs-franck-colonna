8fe7739be216015724c71e10874f995e3a6aa19d
Franck Colonna <contact@franckcolonna.fr>
2021-11-19 13:49:42 +0100

    ajout forcast

--- cityservice/src/main/java/com/m2dfs/cityservice/controller/CityController.java
+++ cityservice/src/main/java/com/m2dfs/cityservice/controller/CityController.java
@@ -32,6 +32,43 @@ public class CityController {
         return response = restTemplate.exchange("http://localhost:8082/current/"+key,
                 HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, key).getBody();
     }
+
+    @ApiOperation(value = "Get 5 day forecast", response = CityController.class, tags = "get5DayForecast")
+    @RequestMapping(value = "/prevision/5/{query}", method = RequestMethod.GET)
+    public String get5DayForecast(@PathVariable String query) {
+        String response = restTemplate.exchange("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=Rylikb8YkhPmbdXO2aAsTAfs97NsPpBc&q=" + query + "&language=fr-fr",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, query).getBody();
+
+        String json = response;
+        json = json.substring(1, json.length() - 1);
+        Map jsonJavaRootObject = new Gson().fromJson(json, Map.class);
+
+        String result = (String) jsonJavaRootObject.get("Key");
+
+        String finalResponse = restTemplate.exchange("http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + result + "?apikey=Rylikb8YkhPmbdXO2aAsTAfs97NsPpBc&language=fr-fr",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, result).getBody();
+
+        return finalResponse;
+    }
+
+    @ApiOperation(value = "Get 1 day forecast", response = CityController.class, tags = "get1DayForecast")
+    @RequestMapping(value = "/prevision/1/{query}", method = RequestMethod.GET)
+    public String get1DayForecast(@PathVariable String query) {
+        String response = restTemplate.exchange("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=Rylikb8YkhPmbdXO2aAsTAfs97NsPpBc&q=" + query + "&language=fr-fr",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, query).getBody();
+
+        String json = response;
+        json = json.substring(1, json.length() - 1);
+        Map jsonJavaRootObject = new Gson().fromJson(json, Map.class);
+
+        String result = (String) jsonJavaRootObject.get("Key");
+
+        String finalResponse = restTemplate.exchange("http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + result + "?apikey=Rylikb8YkhPmbdXO2aAsTAfs97NsPpBc&language=fr-fr",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, result).getBody();
+
+        return finalResponse;
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
