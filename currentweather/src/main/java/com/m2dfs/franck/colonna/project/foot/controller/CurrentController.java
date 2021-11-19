0afa28c0ef5ecb7d1f38e225ceaadbbbfff51d21
Franck Colonna <contact@franckcolonna.fr>
2021-11-19 10:42:20 +0100

    current weather service

---
+++ currentweather/src/main/java/com/m2dfs/franck/colonna/project/foot/controller/CurrentController.java
@@ -0,0 +1,31 @@
+package com.m2dfs.franck.colonna.project.foot.controller;
+
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class CurrentController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @ApiOperation(value = "Get details for weather", response = CurrentController.class, tags = "getWeather")
+    @RequestMapping(value = "/current/{key}", method = RequestMethod.GET)
+    public String getWeather(@PathVariable String key) {
+        String response = restTemplate.exchange("http://dataservice.accuweather.com/currentconditions/v1/{key}?apikey=u5S7y7V5wFQR7JlNwu4G0Y9x4pOXJY2b&language=fr-fr",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, key).getBody();
+        return response;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
