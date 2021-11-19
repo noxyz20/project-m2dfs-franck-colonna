4c6267d9c24fe6dfe1860956be9ceeca4d351b03
Franck Colonna <contact@franckcolonna.fr>
2021-11-19 11:27:32 +0100

    Getcity

--- currentweather/src/main/java/com/m2dfs/franck/colonna/project/foot/controller/CurrentController.java
+++ currentweather/src/main/java/com/m2dfs/franck/colonna/project/foot/controller/CurrentController.java
@@ -24,6 +24,16 @@ public class CurrentController {
         return response;
     }
 
+    String query;
+
+    @ApiOperation(value = "Get details for weather", response = CurrentController.class, tags = "getCity")
+    @RequestMapping(value = "/getCity/{query}", method = RequestMethod.GET)
+    public String getCity(@PathVariable String query) {
+        String response = restTemplate.exchange("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=2ISwGQTvWNt4926KkCQnSbTUXDoWTXCc&q=" + query + "&language=fr-fr",
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, query).getBody();
+        return response;
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
